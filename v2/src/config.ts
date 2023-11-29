1ea8f0b3f6c6369fa122f2b26a1326e17c6ce882
Robin Papa <mail@robinpapa.nl>
2023-11-29 21:45:31 +0100

    Fixed Twitter env

--- v2/src/config.ts
+++ v2/src/config.ts
@@ -3,7 +3,7 @@
 
 export const SITE_TITLE = "Robin Papa | Process Engineer";
 export const SITE_DESCRIPTION = "Robin develops applications for every business process."
-// export const TWITTER_HANDLE = "";
+export const TWITTER_HANDLE = "";
 export const MY_NAME = "Robin Papa";
 
 // setup in astro.config.mjs
