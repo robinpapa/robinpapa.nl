9a2ebaf4194fd47447429ba109111832faaffd82
Robin Papa <mail@robinpapa.nl>
2023-12-03 13:01:52 +0100

    edited favicon and now showing dark mode toggle

--- v2/src/config.ts
+++ v2/src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Robin Papa | Process Engineer";
+export const SITE_TITLE = "Robin Papa | Workflow Architect";
 export const SITE_DESCRIPTION = "Robin develops applications for every business process."
 export const MY_NAME = "Robin Papa";
 
