08888a4d0d60df5280e37eae12983e4beee932a6
Robin Papa <mail@robinpapa.nl>
2024-06-02 17:17:01 +0200

    fixed personal website

---
+++ robinpapa/.vercel/output/static/_astro/hoisted.egeCYbU5.js
@@ -0,0 +1,1 @@
+import"./hoisted.m1JmapqD.js";const r=document.querySelectorAll("a#copyEmail");r.forEach(e=>{e.addEventListener("click",()=>{navigator.clipboard.writeText("robin@superwork.nl"),e.innerText="Emailadres gekopieerd"})});
